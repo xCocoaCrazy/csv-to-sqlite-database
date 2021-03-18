242baee3c40a444cb681de8c05b722650047cbcf
xcocoacrazy <xcocoacrazy13@gmail.com>
2021-03-18 14:42:40 +0200

    Added the reading from CSV and the main.

--- CSV-SQLITE/src/rosca/main/Main.java
+++ CSV-SQLITE/src/rosca/main/Main.java
@@ -1,4 +1,24 @@
 package rosca.main;
 
+
+import rosca.classes.ReadFromCSV;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
+
 public class Main {
+    public static void main(String[] args) {
+        try {
+            ReadFromCSV read = new ReadFromCSV("test");
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+        } catch (FileNotFoundException e) {
+            System.err.println("Oops, file not found!");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.err.println("Oops, problems with file!");
+            e.printStackTrace();
+        }
+
+    }
 }
