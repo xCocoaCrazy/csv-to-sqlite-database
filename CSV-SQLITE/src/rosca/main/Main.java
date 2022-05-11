6f0d53dbcd56900d18f8755e1de53205dfa4d515
xcocoacrazy <xcocoacrazy13@gmail.com>
2022-05-11 06:46:07 +0300

    Added some comments.

--- CSV-SQLITE/src/rosca/main/Main.java
+++ CSV-SQLITE/src/rosca/main/Main.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.SQLException;
 
+//Main class
 public class Main {
     public static void main(String[] args) {
         try {
